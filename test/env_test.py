7dc97cfb030f5715938b63a4a8e94f02baaac210
Edouard Leurent <eleurent@gmail.com>
2018-05-10 11:47:45 +0200

    Monitor environment

--- test/env_test.py
+++ test/env_test.py
@@ -1,19 +1,24 @@
 from __future__ import print_function, division
 
 import gym
+from gym.wrappers import Monitor
 import obstacle_env
 
 
-def test():
+def test(episodes=3):
     env = gym.make('obstacle-v0')
+    monitor = Monitor(env, directory='out', force=True)
+    for i in range(episodes):
+        print(i)
+        observation = monitor.reset()
+        done = False
+        while not done:
+            action = env.action_space.sample()
+            observation, reward, done, info = monitor.step(action)
+            monitor.render()
+            # print(observation)
+    monitor.close()
 
-    observation = env.reset()
-    actions = [1, 1, 4]*10
-    for i in range(len(actions)):
-        observation, reward, done, info = env.step(actions[i])
-        env.render()
-        print(env.dynamics.state)
 
-
-if __name__ == "main":
+if __name__ == "__main__":
     test()
