93db0d18a9447d2aa038e2b77a22b29f256a136e
Edouard Leurent <eleurent@gmail.com>
2020-01-31 18:06:54 +0100

    Add LPV attribute to step a set of dynamics and compute pessimistic rewards

--- obstacle_env/envs/obstacle.py
+++ obstacle_env/envs/obstacle.py
@@ -13,10 +13,13 @@ from obstacle_env.scene import Scene2D, PolarGrid
 class ObstacleEnv(gym.Env):
     metadata = {'render.modes': ['human', 'rgb_array']}
 
-    SIMULATION_FREQUENCY = 10
-    POLICY_FREQUENCY = 2
-    GRID_CELLS = 16
-    COLLISION_REWARD = -1
+    config = {
+        "simulation_frequency": 10,
+        "policy_frequency": 2,
+        "grid_cells": 16,
+        "collision_reward": -0.5,
+        "observation_type": "grid"
+    }
 
     def __init__(self):
         # Seeding
@@ -24,14 +27,14 @@ class ObstacleEnv(gym.Env):
         self.seed()
 
         # Dynamics
-        params = Dynamics2D2.DEFAULT_PARAMS.update({'dt': 1 / self.SIMULATION_FREQUENCY})
+        params = Dynamics2D2.DEFAULT_PARAMS.update({'dt': 1 / self.config["simulation_frequency"]})
         self.dynamics = Dynamics2D2(params=params)
         self.dynamics.desired_action = self.np_random.randint(1, len(self.dynamics.ACTIONS))
 
         # Scene
         self.scene = Scene2D()
         self.scene.create_random_scene(np_random=self.np_random)
-        self.grid = PolarGrid(self.scene, cells=self.GRID_CELLS)
+        self.grid = PolarGrid(self.scene, cells=self.config["grid_cells"])
 
         # Spaces
         self.action_space = spaces.Discrete(len(self.dynamics.ACTIONS))
@@ -50,8 +53,17 @@ class ObstacleEnv(gym.Env):
         self.rendering_mode = 'human'
         self.enable_auto_render = False
 
+        self.lpv = None
+
         self.steps = 0
+        self.time = 0
         self.done = False
+        self.trajectory = []
+        self.interval_trajectory = []
+
+    def configure(self, config):
+        self.config.update(config)
+        self.reset()
 
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
@@ -63,6 +75,7 @@ class ObstacleEnv(gym.Env):
         :return: the observation of the reset state
         """
         self.steps = 0
+        self.time = 0
         self.scene.create_random_scene(np_random=self.np_random)
         self.dynamics.state *= 0
         self.dynamics.crashed = False
@@ -78,13 +91,20 @@ class ObstacleEnv(gym.Env):
         :param int action: the action performed
         :return: a tuple (observation, reward, terminal, info)
         """
-        # Forward action to the dynamics
-        self.dynamics.act(action)
-
         # Simulate
-        for k in range(int(self.SIMULATION_FREQUENCY // self.POLICY_FREQUENCY)):
+        for k in range(int(self.config["simulation_frequency"] // self.config["policy_frequency"])):
+            if action is not None and \
+                    self.time % int(self.config["simulation_frequency"] // self.config["policy_frequency"]) == 0:
+                # Forward action to the dynamics
+                self.dynamics.act(action)
+
+            self.store_data()
             self.dynamics.step()
             self.dynamics.check_collisions(self.scene)
+            if self.lpv:
+                self.lpv.set_control((self.dynamics.B @ self.dynamics.control).squeeze(-1))
+                self.lpv.step(1 / self.config["simulation_frequency"])
+            self.time += 1
 
             # Render simulation
             self._automatic_rendering()
@@ -94,7 +114,6 @@ class ObstacleEnv(gym.Env):
                 break
 
         self.enable_auto_render = False
-
         self.steps += 1
         obs = self._observation()
         reward = self._reward(action)
@@ -166,14 +185,17 @@ class ObstacleEnv(gym.Env):
             - a vector of normalized range measurements in N directions
         :return: the observation
         """
-        velocities = self.dynamics.velocity / self.dynamics.terminal_velocity
-        desired_control = self.dynamics.action_to_control(self.dynamics.desired_action) / \
-                          self.dynamics.params['acceleration']
+        if self.config["observation_type"] == "grid":
+            velocities = self.dynamics.velocity / self.dynamics.terminal_velocity
+            desired_control = self.dynamics.action_to_control(self.dynamics.desired_action) / \
+                              self.dynamics.params['acceleration']
 
-        ranges = self.grid.trace(self.dynamics.position)
-        ranges = np.minimum(ranges, self.grid.MAXIMUM_RANGE) / self.grid.MAXIMUM_RANGE
-        observation = np.vstack((velocities, desired_control, ranges))
-        return np.ravel(observation)
+            ranges = self.grid.trace(self.dynamics.position)
+            ranges = np.minimum(ranges, self.grid.MAXIMUM_RANGE) / self.grid.MAXIMUM_RANGE
+            observation = np.vstack((velocities, desired_control, ranges))
+            return np.ravel(observation)
+        else:
+            return self.dynamics.state
 
     def _reward(self, action):
         """
@@ -182,6 +204,8 @@ class ObstacleEnv(gym.Env):
         :param action: the last action performed
         :return: the reward
         """
+        if self.lpv is not None:
+            return self.pessimistic_reward(self.lpv.x_i_t)
         if self.scene.goal:
             d = np.linalg.norm(self.scene.goal["position"] - self.dynamics.position)
             d0 = 10
@@ -190,13 +214,45 @@ class ObstacleEnv(gym.Env):
             desired_control = self.dynamics.action_to_control(self.dynamics.desired_action)
             control = self.dynamics.action_to_control(action)
             reward = (1 - np.linalg.norm(desired_control - control) / (2 * self.dynamics.params['acceleration'])) ** 2
-        return remap(reward + self.COLLISION_REWARD * self.dynamics.crashed, [-1, 1], [0, 1])
+        return remap(reward + self.config["collision_reward"] * self.dynamics.crashed, [-1, 1], [0, 1])
+
+    def pessimistic_reward(self, interval):
+        """
+            Return the reward associated with performing a given action and ending up in the current state.
+
+        :param interval: a state interval [min, max]
+        :return: the reward
+        """
+        reward = 0
+        collision = self.pessimistic_is_terminal(interval)
+        corners = [[interval[0, 0], interval[0, 1]],
+                   [interval[0, 0], interval[1, 1]],
+                   [interval[1, 0], interval[0, 1]],
+                   [interval[1, 0], interval[1, 1]]]
+        if self.scene.goal:
+            d = np.linalg.norm(self.scene.goal["position"] - np.mean(corners, axis=0)[:, np.newaxis])
+            d0 = 10
+            reward = 1/(1+d/d0)
+        return remap(reward + self.config["collision_reward"] * collision, [-1, 1], [0, 1])
+
+    def pessimistic_is_terminal(self, interval):
+        corners = np.array([[interval[0, 0], interval[0, 1]],
+                   [interval[0, 0], interval[1, 1]],
+                   [interval[1, 0], interval[0, 1]],
+                   [interval[1, 0], interval[1, 1]]])
+        for position in corners:
+            for obstacle in self.scene.obstacles:
+                if np.linalg.norm(position[:, np.newaxis] - obstacle['position']) < obstacle['radius']:
+                    return True
+        return False
 
     def _is_terminal(self):
         """
             Check whether the current state is a terminal state
         :return:is the state terminal
         """
+        if self.lpv:
+            return self.pessimistic_is_terminal(self.lpv.x_i_t)
         return self.dynamics.crashed
 
     def __deepcopy__(self, memo):
@@ -226,6 +282,11 @@ class ObstacleEnv(gym.Env):
                            if np.linalg.norm(obstacle['position'] - self.dynamics.position) < 2*state_copy.grid.MAXIMUM_RANGE]
         state_copy.scene.obstacles = obstacles_close
         return state_copy
+
+    def store_data(self):
+        self.trajectory.append(self.dynamics.state)
+        if self.lpv:
+            self.interval_trajectory.append(self.lpv.x_i_t)
 
 
 def remap(v, x, y, clip=False):
