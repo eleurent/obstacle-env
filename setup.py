a2204e81b49e237c1dde44c3f5f3868dee74e39f
Edouard Leurent <eleurent@gmail.com>
2020-10-01 11:01:25 +0200

    Drop python 2 support

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Researchers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
     ],
 
